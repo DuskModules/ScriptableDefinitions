d1e9db48e0037b81f834bb23f250303e9f97b24d
Eric van Gastel <eric@dusklightstudios.com>
2019-05-20 23:40:34 +0200

    Added info in DefinitionEditor

---
+++ Scripts/Editor/DefinitionObjectEditor.cs
@@ -0,0 +1,23 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace DuskModules.ScriptingDefinitions.DuskEditor {
+
+	/// <summary> Editor for game event </summary>
+	[CustomEditor(typeof(DefinitionObject))]
+	[CanEditMultipleObjects]
+	public class DefinitionObjectEditor : Editor {
+
+		/// <summary> Inspector GUI </summary>
+		public override void OnInspectorGUI() {
+			base.OnInspectorGUI();
+
+			EditorGUILayout.HelpBox("When a Definition Object is imported into a Unity Project, it adds it's own name as a Scripting Definition Symbol. " +
+				"When it's removed, it removes the symbol.", MessageType.Info);
+		}
+	}
+}
+#endif
